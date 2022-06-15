7cc20b3cf1f4a9b5595d2e3f91011280361cd5ce
saff-it <saff_it@ymail.com>
2022-06-15 16:09:53 +0200

    milestone 1

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 // Variabili
-const userDistance = parseInt( document.querySelector('user-km-num') );
-const userAge = parseInt( document.querySelector('user-age') );
-let ticketPrice = userDistance * 0.26;
+const calcButton = document.getElementById("calc-button");
 
-console.log(userDistance, userAge, ticketPrice);
+calcButton.addEventListener('click', function() {
+    const userDistance = parseInt( document.querySelector('user-km-num') );
+    const userAge = parseInt( document.querySelector('user-age') );
+    let ticketPrice = userDistance * 0.26;
 
-// Document
-// document.getElementById('user-distance').innerHTML = userDistance;
-// document.getElementById('user-age').innerHTML = userAge;
+    console.log(userDistance, userAge, ticketPrice);
 
 
-// Conditions
+    if ( isNaN(userDistance) == true ) {
+        console.log('Qualcosa è andato storto; ricarica la pagina ed inserisci il numero di KM che intendi percorrere');
+    
+    } else if ( isNaN(userAge) == true ) {
+        console.log('Qualcosa è andato storto; ricarica la pagina ed inserisci il numero dei tuoi anni');
+    
+    } else {
+    
+        if (userAge < 18) {
+            ticketPrice = (ticketPrice - ( (ticketPrice * 15) / 100) );
+            
+            
+        } else if (userAge > 65) {
+            ticketPrice = (ticketPrice - ( (ticketPrice * 35) / 100) );
+        }
+    
+        document.getElementById('ticket-price').innerHTML = ticketPrice.toFixed( 2 );
+    }    
 
-if ( isNaN(userDistance) == true ) {
-    console.log('Qualcosa è andato storto; ricarica la pagina ed inserisci il numero di KM che intendi percorrere');
+});
 
-} else if ( isNaN(userAge) == true ) {
-    console.log('Qualcosa è andato storto; ricarica la pagina ed inserisci il numero dei tuoi anni');
 
-} else {
 
-    if (userAge < 18) {
-        ticketPrice = (ticketPrice - ( (ticketPrice * 15) / 100) );
-        
-        
-    } else if (userAge > 65) {
-        ticketPrice = (ticketPrice - ( (ticketPrice * 35) / 100) );
-    }
 
-    document.getElementById('ticket-price').innerHTML = ticketPrice.toFixed( 2 );
-}    
+
+
+
+
+
+
+
+
 
